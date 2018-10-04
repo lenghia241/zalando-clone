4e10c94e53d8764df0452dce6944ae8cd4015d33
Le Nghia <nghia.neal@gmail.com>
2018-10-04 10:42:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            src={require("./assets/logo.png")}
+            className="App-logo"
+            alt="logo"
+          />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
